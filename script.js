7ad99997819b90d41e16c63bd2d5508e81522318
AngeloOL <120499633+zAngelitohXd@users.noreply.github.com>
2026-02-11 23:47:05 -0500

    script.js

--- script.js
+++ script.js
@@ -2,30 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const slider = document.getElementById("slider");
 
-    const totalImagenes = 7; // cantidad de fotos
+    const totalImagenes = 7;
     let indice = 1;
 
     slider.src = "1.jpg";
 
     setInterval(function () {
 
-        slider.style.transform = "rotateY(-90deg)";
+        indice++;
 
-        setTimeout(function () {
+        if (indice > totalImagenes) {
+            indice = 1;
+        }
 
-            indice++;
-            if (indice > totalImagenes) {
-                indice = 1;
-            }
-
-            slider.src = indice + ".jpg";
-            slider.style.transform = "rotateY(0deg)";
-
-        }, 400);
+        slider.src = indice + ".jpg";
 
     }, 3000);
 
 });
+
+
 
 
 
