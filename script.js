80c6b2bc488cece56f9e0fc3dc6b77faff1d6019
AngeloOL <120499633+zAngelitohXd@users.noreply.github.com>
2026-02-11 23:43:15 -0500

    script.js

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const slider = document.getElementById("slider");
 
+    const totalImagenes = 7; // cantidad de fotos
+    let indice = 1;
+
     slider.src = "1.jpg";
 
-});
+    setInterval(function () {
 
+        slider.style.transform = "rotateY(-90deg)";
+
+        setTimeout(function () {
+
+            indice++;
+            if (indice > totalImagenes) {
+                indice = 1;
+            }
+
+            slider.src = indice + ".jpg";
+            slider.style.transform = "rotateY(0deg)";
+
+        }, 400);
+
+    }, 3000);
+
+});
 
 
 
