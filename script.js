74cd0aae073c381790d8c6e2b1aa19bdb5fb7245
AngeloOL <120499633+zAngelitohXd@users.noreply.github.com>
2026-02-11 23:49:48 -0500

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const slider = document.getElementById("slider");
-
-    const totalImagenes = 7;
-    let indice = 1;
-
-    slider.src = "1.jpg";
-
-    setInterval(function () {
-
-        indice++;
-
-        if (indice > totalImagenes) {
-            indice = 1;
-        }
-
-        slider.src = indice + ".jpg";
-
-    }, 3000);
+    alert("JS funcionando");
 
 });
+
+
 
 
 
